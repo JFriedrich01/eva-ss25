07200b3429c4f5a98ea0ab44521c0a1dd39824ac
Friedrich <jojofrie55@gmail.com>
2025-04-10 13:36:41 +0200

    add comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    // Hauptmethode: Einstiegspunkt des Programms
     public static void main(String[] args) {
+        // Gibt den Text "Hello world!" in der Konsole aus
         System.out.println("Hello world!");
     }
 }
